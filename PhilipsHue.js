c3965a8549c8c80787ec6f94b73d365270c450cc
Aaron <aaronramshaw@gmail.com>
2016-09-11 15:00:38 +1000

    moved API stuff to PhilipsHue.js

---
+++ PhilipsHue.js
@@ -0,0 +1,43 @@
+const request = require('request');
+
+class PhilipsHue {
+  constructor(bridgeIP, username) {
+    this.bridgeIP = bridgeIP;
+    this.username = username;
+    this.lights = [];
+  }
+
+  initialiseLights(done) {
+    const getLightsURL = this.bridgeIP + '/api' + this.username + '/lights';
+
+    request.get({
+      url: getLightsURL,
+    }, (error, response) => {
+      const lights = JSON.parse(response.body);
+      this.lights = Object.keys(lights);
+      console.log('Initialisation complete');
+      done();
+    });
+  }
+
+  lightsList() {
+    return this.lights;
+  }
+
+  setLights(id, value) {
+    const setLightsURL = this.bridgeIP + '/api' + this.username + '/lights' + id + '/state';
+    const command  = {
+      'on': value
+    };
+    request.put({
+      url: setLightsURL,
+      json: command,
+    }, (error, response) => {
+      if (error) {
+        console.log('Failed to set light ' + id);
+      }
+    });
+  }
+}
+
+module.exports = PhilipsHue;
--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const clapDetector = require('clap-detector');
-const request = require('request');
 const config = require('config.json')('./config.json');
+const PhilipsHue = require('./PhilipsHue.js');
 
-const userConfig = {
-  user: config.username,
-  api: config.bridge + '/api'
-};
+const philipsHue = new PhilipsHue(config.bridge, config.username);
+philipsHue.initialiseLights(beginClap);
 
 const clapConfig = {
   AUDIO_SOURCE: config.audioSource,
@@ -13,17 +11,6 @@ const clapConfig = {
 };
 
 let state = false;
-let ids = {};
-
-request.get({
-  url: userConfig.api + userConfig.user + '/lights'
-}, (error, response) => {
-  const lights = JSON.parse(response.body);
-  ids = Object.keys(lights);
-  console.log('Initialisation complete');
-  beginClap();
-});
-
 
 function beginClap() {
   clapDetector.start(clapConfig);
@@ -31,29 +18,17 @@ function beginClap() {
 }
 
 function toggleLights() {
-  setLights(ids, state);
-  state = !state;
-}
+  const ids = philipsHue.lightsList();
 
-function setLights(ids, on) {
-  setTimeout(() => {
-    if (on) {
-      console.log('Turning on lights ' + ids);
-    } else {
-      console.log('Turning off lights ' + ids);
-    }
-    for (const i of ids) {
-      const onCommand = {
-        'on': on 
-      };
-      request.put({
-        url: userConfig.api + userConfig.user + '/lights/' + i + '/state',
-        json: onCommand
-      }, (error, response) => {
-        if (error) {
-          console.log('Failed to set light ' + i);
-        }
-      });
-    }
-  });
+  if (state) {
+    console.log('Turning on lights ' + ids);
+  } else {
+    console.log('Turning off lights ' + ids);
+  }
+
+  for (const i of ids) {
+    philipsHue.setLights(i, state);
+  }
+
+  state = !state;
 }
